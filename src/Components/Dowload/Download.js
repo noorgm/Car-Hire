91be0865064617b95e0367e4ec7ce630dfc0b53a
noorgm <97991519+noorgm@users.noreply.github.com>
2023-07-03 15:18:18 +0100

    Finally Downlod Component with its links are done

--- src/Components/Dowload/Download.js
+++ src/Components/Dowload/Download.js
@@ -10,11 +10,11 @@ const Download = () => {
     <section className="download">
         <div className="container download-bg">
             <div className="download-left">
-                <h2>Way to hire a car very easily</h2>
+                <h2>Hire a car with few clicks only</h2>
                 <p>You can download our app and then right away with few clicks you will be able to hire a car with us.</p>
                 <div className="download-store">
-                    <img src={google} alt="google store" />
-                    <img src={apple} alt="apple store" />
+                    <a href='https://play.google.com/store/apps?hl=en_GB&gl=Uk' rel='noreferrer' target='_blank' ><img src={google} alt="google store" /></a>
+                    <a href='https://www.apple.com/uk/app-store/' rel='noreferrer' target='_blank' ><img src={apple} alt="apple store" /></a>                   
                 </div>
             </div>
         </div>
