f0ea6d0916293a93271dcc810fc8e15236345762
noorgm <97991519+noorgm@users.noreply.github.com>
2023-07-05 18:22:41 +0100

    Sorted the Team page and fix soem more stuffs here and there a little bit changes

---
+++ src/Assets/Data/team.js
@@ -0,0 +1,33 @@
+import ceo from '../faces/ceo.png';
+import manager from '../faces/manager.png';
+import supervisor from '../faces/supervisor.png';
+import cardetailist from '../faces/car-detailist.png';
+import saless from '../faces/saless.png';
+import sales from '../faces/sales.png';
+
+
+
+const team = [
+    {
+       id: 0, fullName: 'Murad Beg', job: 'CEO', img: ceo
+    }, 
+    {
+       id: 1, fullName: 'Nick Jones', job: 'Manager', img: manager
+    }, 
+    {
+       id: 2, fullName: 'Mokhtar Jackson', job: 'Supervisor', img: supervisor
+    }, 
+    {
+       id: 3, fullName: 'Luke Adam', job: 'Cer Detailist', img: cardetailist
+    }, 
+    {
+       id: 4, fullName: 'Amy Smith', job: 'Sales Assistant', img: saless
+    }, 
+    {
+       id: 5, fullName: 'Sophie Miller', job: 'Sales Assistant', img: sales
+    }
+]
+
+
+
+export default team;
--- src/Pages/Team/Team.js
+++ src/Pages/Team/Team.js
@@ -1,10 +1,44 @@
 import React from 'react';
 
+import team from '../../Assets/Data/team';
 import './team.css';
 
 const Team = () => {
   return (
-    <div>Team</div>
+    <main className="team-bg">
+
+      <section className="top-team-bg">
+        <div className="top-team container">
+          <h2>Why work for us?</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore obcaecati eum maiores Lorem ipsum dolor sit amet consectetur adipisicing elit. In non accusantium assumenda vitae eveniet voluptas dolores laborum velit quidem? Consequatur, libero. Vel, tempora excepturi sunt consequatur inventore nulla deleniti magnam! Tempore quam laudantium pariatur laboriosam voluptatum, dignissimos debitis voluptatibus exercitationem minus explicabo culpa ratione commodi, possimus quos harum, iure consequatur beatae ab aliquid? praesentium, exercitationem, ad minus consequuntur tempora dolore, deserunt nam officia unde quam! Officiis totam temporibus, quos ex similique soluta.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit hic quam perspiciatis harum dicta tempora ipsam amet.</p>
+          <ul>
+            <li>Lorem ipsum dolor sit amet.</li>
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime!</li>
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+            <li>Lorem ipsum dolor sit amet.</li>
+            <li>Lorem ipsum dolor, sit amet consectetur adipisicing.</li>
+          </ul>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Culpa totam quo quisquam, velit corrupti magnam aliquam dolore quae maiores. Placeat expedita corporis molestiae! Dolores.</p>
+        </div>
+      </section>
+
+      <h1 className='meet-team'>Meet the team</h1>
+      <section className="end-team container">
+        {
+          team.map((teams) => (
+            <div key={teams.id} className="team-box">
+              <img src={teams.img} alt="ceo" />
+              <div className="team-box-info">
+                <h4>{teams.fullName}</h4>
+                <p>{teams.job}</p>
+              </div>
+            </div>
+          ))
+        }
+      </section>
+      
+    </main>
   )
 }
 
