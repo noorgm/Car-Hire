8b057d4738fa2e9e76e7d39062b84f0e91ef69f1
noorgm <97991519+noorgm@users.noreply.github.com>
2023-07-05 15:59:58 +0100

    Sorted the Contact Page

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -8,57 +8,35 @@ import './contact.css';
 const Contact = () => {
   return (
     <main className="contact">
-      <div className="contact-start">
+     <div className="contact-start">
         <h1>Contact</h1>
         <p>Contact our friendly team and let us know how we can help you.</p>
-      </div>
-      <div className="contact-main container">
-        <div className="contact--left">
-            <div className="contact-box">
-                <div className="contact-icon">
-                  <QuestionAnswerOutlinedIcon />
+     </div>
+     <div className="contact-bg">
+        <div className="contact-main container">
+            <div className="left-contact">
+                <div className="chat-contact">
+                    <h3><span><QuestionAnswerOutlinedIcon className='contact-icon' /></span> Chat to us</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur.</p>
+                    <p>info@hireacar.com</p>
                 </div>
-                <div className="contact-below-icon">
-                    <h1>Chat to us</h1>
-                    <p>Lorem ipsum, dolor sit amet adipisicing.</p>
-                    <p>loreme@gmail.com</p>
-                </div>                
-            </div>
-            <div className="contact-box-one">
-                <div className="contact-icon">
-                    <BusinessOutlinedIcon />
+                <div className="visit-contact">
+                    <h3><span><BusinessOutlinedIcon className='contact-icon' /></span> Visit us</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur.</p>
+                    <p>Lorem Townfield <br /> DWE LS99 7TY</p>
                 </div>
-                <div className="contact-below-icon">
-                    <h1>Visit us</h1>
-                    <p>Lorem ipsum, dolor sit amet adipisicing.</p>
-                    <p>Lorem Townfield DWE LS99 7TY</p>
-                </div>                   
-            </div>
-            <div className="contact-box-two">
-                <div className="contact-icon">
-                    <AddIcCallOutlinedIcon />
-                </div>
-                <div className="contact-below-icon">
-                    <h1>Call us</h1>
-                    <p>Mon-Sun Anytime Of the week</p>
+                <div className="chat-contact">
+                    <h3><span><AddIcCallOutlinedIcon className='contact-icon' /></span> Call us</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur.</p>
                     <p>0113 571 3173</p>
                 </div>
             </div>
-         </div>
 
-        <div className="contact-right-bg">
-            <div className="contact-right-btop">
-                <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus neque autem reprehenderit, voluptate saepe sint repellendus dolore aut molestias nobis, unde sit consequatur!</h3> 
-            </div>           
-            <div className="contact-right-bottom">
-                <h3>Noorullah GM</h3> 
-                <div>
-                    <h3>Company Logo</h3>
-                </div>
+            <div className="right-contact">
+                <h3>Created and Designed by Noorullah GM</h3>
             </div>
-        </div>
-
-      </div>
+        </div>        
+     </div>
     </main>
   )
 }
