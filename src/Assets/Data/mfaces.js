e2d9f77bf59c2124a9a2c65d3e9768924f087e9e
noorgm <97991519+noorgm@users.noreply.github.com>
2023-07-10 19:17:46 +0100

    Finished the Mobile Version of review componet

---
+++ src/Assets/Data/mfaces.js
@@ -0,0 +1,16 @@
+import young from '../faces/face8.svg';
+import homed from '../faces/face9.svg';
+
+
+
+const mobileFaces = [
+    {
+        id: 0, name: 'Young', surName: 'Lei', img: young , city: 'Beijing', comment: 'Lorem ipsum dolor sit amet, consecteturconsectetur consectetur adipiscing elit. Pellentesque placerat nisi quis felis pretium, quis mattis quam rhoncus. Integer quis pretium.'
+    }, 
+    {
+        id: 1, name: 'Homed', surName: 'Ali', img: homed , city: 'Liverpool', comment: 'Lorem ipsum dolor sit amet, consecteturconsectetur consectetur adipiscing elit. Pellentesque placerat nisi quis felis felis felis felis pretium, quis mattis quam rhoncus. Integer quis pretium.'
+    }
+]
+
+
+export default mobileFaces;
--- src/Components/Review/Review.js
+++ src/Components/Review/Review.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import faces from '../../Assets/Data/faces';
+import mobileFaces from '../../Assets/Data/mfaces';
 import glass from '../../Assets/images/glassdoor.png';
 import google from '../../Assets/images/google.png';
 import trust from '../../Assets/images/trustpilot.png';
 import StarIcon from '@mui/icons-material/Star';
-import faces from '../../Assets/Data/faces';
 import left from '../../Assets/images/left-quote.png';
 import right from '../../Assets/images/right-quote.png';
 
@@ -49,7 +50,7 @@ const Review = () => {
                 review.map(face => (
                   <div key={face.id} className="review-box" >
                     <img src={left} alt="left-quote" />
-                    <span className='face-commment'>{face.comment}</span>
+                    <span className='face-comment'>{face.comment}</span>
                     <img src={right} alt="right-quote" />
                     <div className="review-bottom">
                       <img src={face.img} alt="faces" />
@@ -65,6 +66,27 @@ const Review = () => {
           ))
         }
       </Carousel>
+
+      {/* For Mobile Screen only */}
+      <section className="grid">
+        {
+          mobileFaces.map((mfaces) => (
+            <div className="card-mfaces" key={mfaces.id}>
+              <img src={left} alt="left-quote" className='m-quote' />
+              <span className='face-comment'>{mfaces.comment}</span>
+              <img src={right} alt="right-quote" className='m-quote' />
+              <div className="mfaces-bottom">
+                <img src={mfaces.img} alt="mfacess" />
+                <div className="mfaces-bott">
+                  <h3>{mfaces.name} {mfaces.surName}</h3>
+                  <p>{mfaces.city}</p>
+                </div>
+              </div>
+            </div>
+          ))
+        }
+      </section>
+
     </section>
   )
 }
