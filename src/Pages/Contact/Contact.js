fd2ba83a94e9705a45bc037b42b0155567dc5c30
noorgm <97991519+noorgm@users.noreply.github.com>
2023-07-16 23:33:54 +0100

    Sorted all the Pages, Need to do the Nav mobile version n some small thigns

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -23,35 +23,41 @@ const Contact = () => {
 
   return (
     <main className="contact">
-      <section className="contact-start">
-        <h1>Contact</h1>
-        <p>Contact our friendly team and let us know how we can help you.</p>
+
+      <section className="top-contact-bg">
+        <div className="contact-start">
+          <h1>Contact</h1>
+          <p>Contact our friendly team and let us know how we can help you.</p>
+        </div>
       </section>
 
-      <section className="grid">
-      <artitle className="contact-bg" id='contact-id'>
-        <div className="chat-contact">
-          <h3><span><QuestionAnswerOutlinedIcon className='contact-icon' /></span> Chat to us</h3>
-          <p>Lorem ipsum dolor sit amet consectetur.</p>
-          <p>info@hireacar.com</p>
-        </div>
+      <h2>Get in touch with us</h2>
+      <section className="second-contact">
+        <artitle className="contact-bg" id='contact-id'>
+          <div className="chat-contact">
+            <h3><span><QuestionAnswerOutlinedIcon className='contact-icon' /></span> Chat to us</h3>
+            <p>Lorem ipsum dolor sit amet consectetur.</p>
+            <p>info@hireacar.com</p>
+          </div>
 
-        <div className="visit-contact">
-          <h3><span><BusinessOutlinedIcon className='contact-icon' /></span> Visit us</h3>
-          <p>Lorem ipsum dolor sit amet consectetur.</p>
-          <p>Lorem Townfield <br /> DWE LS99 7TY</p>
-        </div>
+          <div className="visit-contact">
+            <h3><span><BusinessOutlinedIcon className='contact-icon' /></span> Visit us</h3>
+            <p>Lorem ipsum dolor sit amet consectetur.</p>
+            <p>Lorem Townfield <br /> DWE LS99 7TY</p>
+          </div>
 
-        <div className="chat-contact">
-          <h3><span><AddIcCallOutlinedIcon className='contact-icon' /></span> Call us</h3>
-          <p>Lorem ipsum dolor sit amet consectetur.</p>
-          <p>0113 571 3173</p>
-        </div>
-      </artitle>
+          <div className="call-contact">
+            <h3><span><AddIcCallOutlinedIcon className='contact-icon' /></span> Call us</h3>
+            <p>Lorem ipsum dolor sit amet consectetur.</p>
+            <p>0113 571 3173</p>
+          </div>
+        </artitle>
 
-      <artitle className="right-contact">
-        <h3>Created and Designed by Noorullah GM</h3>
-      </artitle>   
+        <artitle className="right-contact">
+          <div className="right-contact-bg">
+           <h3>Created and Designed by Noorullah GM</h3>
+          </div>
+        </artitle>   
       </section>
       
     </main>
--- src/Pages/SignIn/Sign.js
+++ src/Pages/SignIn/Sign.js
@@ -4,7 +4,16 @@ import './sign.css';
 
 const Sign = () => {
   return (
-    <div>Sign</div>
+    <main className="sign-bg">
+      <div className="sign-in grid">
+        <section className="sign-top">
+          <h2>Sign-In / Register</h2>
+        </section>
+        <section className="fixing-part">
+          <h3>Coming Soon</h3>
+        </section>
+      </div>
+    </main>
   )
 }
 
