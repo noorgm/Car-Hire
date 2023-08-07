c355bf091efce7fb95f016c1f0deeb296c7d66fc
noorgm <97991519+noorgm@users.noreply.github.com>
2023-08-07 21:19:44 +0100

    changed the anchor tag from normal rel to this rel - noopener noreferrer

--- src/Components/Dowload/Download.js
+++ src/Components/Dowload/Download.js
@@ -13,8 +13,8 @@ const Download = () => {
         <p>You can download our app and then right away with few clicks you will be able to hire a car with us.</p>
       </div>
       <div className="download-store">
-        <a href='https://play.google.com/store/apps?hl=en_GB&gl=Uk' rel='noreferrer' target='_blank' ><img src={google} alt="google store" /></a>
-        <a href='https://www.apple.com/uk/app-store/' rel='noreferrer' target='_blank' ><img src={apple} alt="apple store" /></a>                   
+        <a href='https://play.google.com/store/apps?hl=en_GB&gl=Uk' rel="noopener noreferrer" target='_blank' ><img src={google} alt="google store" /></a>
+        <a href='https://www.apple.com/uk/app-store/' rel="noopener noreferrer" target='_blank' ><img src={apple} alt="apple store" /></a>                   
       </div>
       <div className="download-right">
         <img src={phone} alt="phone" className='phone-png' />
